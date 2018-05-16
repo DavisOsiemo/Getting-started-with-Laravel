56fb75521527725a146ffccdabe5af5f5defc499
DavisOsiemo <davisossy@gmail.com>
2018-05-16 10:04:27 +0300

    Edited less files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home Page</title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css\ /bootstrap.min.css"> 
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css\ /bootstrap-theme.min.css">
+        <script src="//code.jquery.com/jquery-1.11.3.min.js">
+        </script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js\ ">
+        </script>
+        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Home Page</div>
+                <div class= "quote"> Simplicity is the ultimate sophistication </div>
+            </div>
+        </div>
+    </body>
+</html>
